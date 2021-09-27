298514a4a87b4b197c155228ff3e57321f2dd515
Dan Kallman <dkallman13@gmail.com>
2021-09-27 16:26:14 -0400

    added details page

--- RestaurantRaterRed/Controllers/RestaurantController.cs
+++ RestaurantRaterRed/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterRed.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -58,6 +59,31 @@ namespace RestaurantRaterRed.Controllers
             return RedirectToAction("Index");
         }
         public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
