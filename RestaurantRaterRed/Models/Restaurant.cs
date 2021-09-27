227cd6235f914347d5f4df31d151ed55853ca94c
Dan Kallman <dkallman13@gmail.com>
2021-09-27 10:14:15 -0400

    initial commit

---
+++ RestaurantRaterRed/Models/Restaurant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterRed.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+    public class RestaurantDbContenxt : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
